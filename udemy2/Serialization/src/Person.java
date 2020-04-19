7783ef102c95d1c97edef71ce1e877887d7a0c13
rahul <rahul@elacarte.com>
2020-04-19 13:48:09 -0700

    Section2: Transient and static variables during Serialization

--- udemy2/Serialization/src/Person.java
+++ udemy2/Serialization/src/Person.java
@@ -5,11 +5,17 @@ public class Person implements Serializable {
     private int id;
     private String name;
 
+    // Some variables of class need not be serialized. You can mark those variables as transient
+    // These variables take default values on deserialization. eg int takes the value 0
+    private transient int tempId;
+    // Static variables are NOT included during serialization. They also take default values
+    private static String tempName;
+
     @Override
     public String toString() {
         return "Person{" +
                 "id=" + id +
-                ", name='" + name + '\'' +
+                ", name='" + name + " tempId=" + tempId + " tempName=" + tempName+'\'' +
                 '}';
     }
 
