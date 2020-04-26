1a2b4ad5e28e32f3be4b14498d27eb71d81ee87a
rahul <rahul@elacarte.com>
2020-04-25 19:55:39 -0700

    injecting constant variables via guice. Not recommended way

--- GuiceTut/src/main/java/com/drawingapp/module/AppModule.java
+++ GuiceTut/src/main/java/com/drawingapp/module/AppModule.java
@@ -1,5 +1,7 @@
 package com.drawingapp.module;
 
+import com.drawingapp.requests.SquareRequest;
+import com.drawingapp.requests.SquareRequestSubclass;
 import com.drawingapp.services.DrawShape;
 import com.drawingapp.services.DrawSquare;
 import com.google.inject.AbstractModule;
@@ -7,6 +9,19 @@ import com.google.inject.AbstractModule;
 public class AppModule extends AbstractModule {
     @Override
     protected void configure() {
+        //when abstract class DrawShape  is requested then give the concrete class object of DrawSquare as per the binding rule below
+        //All abstract class or interface must have a binding
         bind(DrawShape.class).to(DrawSquare.class);
+        // Now DrawSquare has 2 variables added which is initialized on construction. So for guice to instantiate the object it must pass some values to constructor
+        //once way to do it as below. but this will make all Strings to Red and integer to 11.. Better way is to use annotations.
+        bind(Integer.class).toInstance(11);
+        bind(String.class).toInstance("Red");
+
+        //when a concrete class is requested, if there is not binding rule, then guice instantiates the object
+        // but if there a binding rule that bind the class to a subclass, then guice instantiates the subclass
+
+        bind(SquareRequest.class).to(SquareRequestSubclass.class);
+
+
     }
 }
--- GuiceTut/src/main/java/com/drawingapp/services/DrawSquare.java
+++ GuiceTut/src/main/java/com/drawingapp/services/DrawSquare.java
@@ -1,10 +1,21 @@
 package com.drawingapp.services;
 
+import com.drawingapp.annotations.ColorValue;
+import com.drawingapp.annotations.EdgeValue;
+
+import javax.inject.Inject;
+
 public class DrawSquare implements DrawShape {
 
-    public DrawSquare() {
-        System.out.println("Creating square object");
+    private Integer edge;
+    private String color;
+
+    @Inject
+    public DrawSquare(Integer edge, String color) {
+        this.edge = edge;
+        this.color = color;
     }
+
     public void draw() {
         System.out.println("Drawing square");
     }
